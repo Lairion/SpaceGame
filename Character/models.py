2a970ce54ba8c4665576a7ee8bee2fa80438ba50
manul <Nunyes_m_a@ukr.net>
2017-09-07 21:20:15 +0300

    Added Ship

--- Character/models.py
+++ Character/models.py
@@ -10,3 +10,17 @@ class Character(models.Model):
 	image= models.ImageField(null=True, blank=True, upload_to='character_image/%Y/%m/%d')
 	def __str__(self):
 		return self.name_character
+class Ship(models.Model):
+	"""docstring for Ship"""
+	SHIP_CLASS = (("DR","DRAGUN"),("HS","HeavyShip"),("MD","Medical"))
+	name_ship = models.CharField(max_length=100)
+	ship_class = models.CharField(max_length=3,choices=SHIP_CLASS,default="DR") 
+	armor = models.IntegerField(default=400)
+class Engine():
+	"""docstring for Engine"""
+	price = models.DecimalField(max_digits=19, decimal_places=2)
+	name_engine = models.CharField(max_length=100)
+	power = models.IntegerField(default=1000)
+	durability = models.IntegerField(default=1000)
+		
+
